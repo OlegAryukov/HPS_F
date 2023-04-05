12a8f414845462e4beb3ec89e2b91e181efc1679
Арюков Олег Нариманович (4080369) <oarukov@dev.vtb.ru>
2023-04-05 11:00:36 +0300

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let X = 5
+let Y = X + 5
+
+// 4.3.1
+printfn "Hello, World!!!"
+
+// 4.3.2
+
+printfn "%A" (2.1 + 3.2 * (8.0 / 4.0 + 1.3))
+
