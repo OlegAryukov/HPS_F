039d83c0ef24279afbaa0ee83ec8b3a6d90c18b1
Арюков Олег Нариманович (4080369) <oarukov@dev.vtb.ru>
2023-04-10 11:16:12 +0300

    TestCase4

--- TestCase3.fs
+++ TestCase3.fs
@@ -1,6 +1,10 @@
+module TestCase3
 let days_in_month = function 
     | 1|3|5|7|9|11 -> 31
     | 4|6|8|10|12 -> 30
     | 2 -> 28
     |_  -> 0
-   
+
+printf $"days in month 1 = {days_in_month 1}"
+printf $"days in month 2 = {days_in_month 2}"
+printf $"days in month 0 = {days_in_month 0}"
