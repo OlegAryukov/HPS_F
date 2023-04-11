4ea23e489a176d4fdb6280bb647489b31ddcd31f
Арюков Олег Нариманович (4080369) <oarukov@dev.vtb.ru>
2023-04-11 16:21:46 +0300

    fix7

--- TestCase6.fs
+++ TestCase6.fs
@@ -1,8 +1,8 @@
 // 17.1
-let rec pow s n =
+let rec pow (s: string, n:int) =
     match n with
     | _ when n <= 0 -> ""
-    | _ -> s + (pow s (n - 1))
+    | _ -> s + (pow (s, (n - 1)))
 
 
 // 17.2
@@ -10,10 +10,10 @@ let rec isIthChar (s: string, n: int, c: char) = s.ToString()[n] = c
 
 // 17.3
 let rec occFromIth (s: string, n: int, c: char) =
-    s |> Seq.filter (fun x' -> x' = c) |> Seq.length
+    s.[..n] |> Seq.filter (fun x' -> x' = c) |> Seq.length
 
 // printfn "%A" (pow ("abC",3))
 //
-// printfn "%A" (isIthChar ("TestCase", 's', 2))
+// printfn "%A" (isIthChar ("TestCase", 2, 's'))
 //
-// printfn "%A" (occFromIth ("TestCase", 's', 1))
+// printfn "%A" (occFromIth ("TestCase", 5, 's'))
