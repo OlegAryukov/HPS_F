478b7757c7d8d5dc3b0b77c30c18146befdbd871
Арюков Олег Нариманович (4080369) <oarukov@dev.vtb.ru>
2023-04-11 17:28:23 +0300

    fix9

--- TestCase6.fs
+++ TestCase6.fs
@@ -12,8 +12,8 @@ let rec isIthChar (s: string, n: int, c: char) = s[n] = c
 let rec occFromIth (s: string, n: int, c: char) =
     s[n..] |> Seq.filter (fun x' -> x' = c) |> Seq.length
 
-printfn "%A" (pow ("abC",3))
-
-printfn "%A" (isIthChar ("TestCase", 2, 's'))
-
-printfn "%A" (occFromIth ("TestCase", 1, 's'))
+// printfn "%A" (pow ("abC",3))
+//
+// printfn "%A" (isIthChar ("TestCase", 2, 's'))
+//
+// printfn "%A" (occFromIth ("TestCase", 1, 's'))
