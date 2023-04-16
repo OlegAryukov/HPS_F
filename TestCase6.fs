3acb069376d82b8427563347baf62e7147cdd8ba
olegaryukov@gmail.com <Gfhjkm23ft6fk!>
2023-04-16 12:08:29 +0300

    fix exception

--- TestCase6.fs
+++ TestCase6.fs
@@ -6,7 +6,10 @@ let rec pow (s: string, n:int) =
 
 
 // 17.2
-let rec isIthChar (s: string, n: int, c: char) = s.[n] = c
+let rec isIthChar (s: string, n: int, c: char) = 
+    match n with
+    | _ when n >=0 && n < s.Length -> s.[n] = c
+    | _ -> false
 
 // 17.3
 let rec occFromIth (s: string, n: int, c: char) =
@@ -14,6 +17,7 @@ let rec occFromIth (s: string, n: int, c: char) =
 
 //printfn "%A" (pow ("abC",3))
 //
-//printfn "%A" (isIthChar ("TestCase", 2, 's'))
+printfn "%A" (isIthChar ("TestCase", 2, 's'))
+printfn "%A" (isIthChar ("TestCase", 10, 's'))
 //
 // printfn "%A" (occFromIth ("TestCase", 1, 's'))
